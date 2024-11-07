6219ffc2da037a1f9f101a2e4518515f7eee7bee
conamiflo <nemanjastjepanovic222@gmail.com>
2024-11-07 19:18:04 +0100

    spring project init

---
+++ backend/WattsGood/src/main/java/com/example/WattsGood/WattsGoodApplication.java
@@ -0,0 +1,13 @@
+package com.example.WattsGood;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WattsGoodApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WattsGoodApplication.class, args);
+	}
+
+}
---
+++ backend/WattsGood/src/test/java/com/example/WattsGood/WattsGoodApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.WattsGood;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WattsGoodApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
