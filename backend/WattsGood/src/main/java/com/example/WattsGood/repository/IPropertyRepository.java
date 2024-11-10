8e6ba9198e97a51e2424e577fb8f2e4096ce69f4
conamiflo <nemanjastjepanovic222@gmail.com>
2024-11-10 11:38:08 +0100

    feature: Added models,repos,services for property registration

---
+++ backend/WattsGood/src/main/java/com/example/WattsGood/repository/IPropertyRepository.java
@@ -0,0 +1,9 @@
+package com.example.WattsGood.repository;
+
+import com.example.WattsGood.model.Property;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface IPropertyRepository extends JpaRepository<Property, String> {
+
+
+}
---
+++ backend/WattsGood/src/main/java/com/example/WattsGood/service/PropertyService.java
@@ -0,0 +1,34 @@
+package com.example.WattsGood.service;
+
+import com.example.WattsGood.model.Property;
+import com.example.WattsGood.repository.IPropertyRepository;
+import com.example.WattsGood.service.interfaces.IPropertyService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class PropertyService implements IPropertyService {
+
+    @Autowired
+    private IPropertyRepository propertyRepository;
+
+    @Override
+    public Property createProperty(Property property) {
+        return propertyRepository.save(property);
+    }
+
+    @Override
+    public List<Property> getAllProperties() {
+        return propertyRepository.findAll();
+    }
+
+    @Override
+    public Optional<Property> getById(Long id) {
+        return propertyRepository.findById(String.valueOf(id));
+    }
+
+
+}
