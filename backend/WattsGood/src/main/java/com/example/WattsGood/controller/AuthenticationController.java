a4fd9e5d1784f36319f9c70b83cc13c7270659e4
stefandjurica69 <stefandjurica69@gmail.com>
2024-11-11 17:30:36 +0100

    Added basic security with jwt

---
+++ backend/WattsGood/src/main/java/com/example/WattsGood/controller/AuthenticationController.java
@@ -0,0 +1,65 @@
+package com.example.WattsGood.controller;
+
+import com.example.WattsGood.dto.LoginDTO;
+import com.example.WattsGood.dto.LoginResponse;
+import com.example.WattsGood.dto.UserDTO;
+import com.example.WattsGood.model.User;
+import com.example.WattsGood.service.JwtService;
+import com.example.WattsGood.service.interfaces.AuthenticationService;
+import com.example.WattsGood.util.exceptions.UserAlreadyExistsException;
+import com.example.WattsGood.util.exceptions.UserNotFoundException;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequestMapping("/api/auth")
+@RestController
+public class AuthenticationController {
+    @Autowired
+    private JwtService jwtService;
+
+    @Autowired
+    private AuthenticationService authenticationService;
+
+    @PostMapping(value = "/register", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<User> register(@RequestBody UserDTO userDTO) {
+        try {
+            User registeredUser = authenticationService.register(userDTO);
+            return new ResponseEntity<>(registeredUser, HttpStatus.CREATED);
+
+        } catch (UserAlreadyExistsException e) {
+            return new ResponseEntity<>(HttpStatus.CONFLICT);
+        }
+    }
+
+    @PostMapping(value = "/login", consumes = MediaType.APPLICATION_JSON_VALUE, produces = MediaType.APPLICATION_JSON_VALUE)
+    public ResponseEntity<LoginResponse> authenticate(@RequestBody LoginDTO loginDTO) {
+        try {
+            User authenticatedUser = authenticationService.authenticate(loginDTO);
+
+            String jwtToken = jwtService.generateToken(authenticatedUser);
+
+            LoginResponse loginResponse = new LoginResponse(jwtToken, jwtService.getExpirationTime());
+
+            if (authenticatedUser.isBlocked()) {
+                return new ResponseEntity<>(HttpStatus.FORBIDDEN);
+            }
+
+            if (!authenticatedUser.isActive()) {
+                return new ResponseEntity<>(loginResponse, HttpStatus.FORBIDDEN);
+            }
+
+            return new ResponseEntity<>(loginResponse, HttpStatus.OK);
+
+        } catch (UserNotFoundException e) {
+            return new ResponseEntity<>(HttpStatus.NOT_FOUND);
+        }
+
+
+    }
+}
