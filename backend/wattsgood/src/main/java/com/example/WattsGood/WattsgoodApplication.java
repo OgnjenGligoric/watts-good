5758511a5ac2a790b661c756894b8ce54b84c424
OgnjenGligoric <gligoric383@gmail.com>
2024-11-11 14:46:15 +0100

    feat: docker compose works for nemanja

--- backend/wattsgood/src/main/java/com/example/WattsGood/WattsgoodApplication.java
+++ backend/wattsgood/src/main/java/com/example/WattsGood/WattsgoodApplication.java
@@ -1,4 +1,4 @@
-package com.example.wattsgood.wattsgood;
+package com.example.wattsgood.WattsGood;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
