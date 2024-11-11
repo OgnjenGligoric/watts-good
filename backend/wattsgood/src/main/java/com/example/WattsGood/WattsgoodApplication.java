3e0a17c6ad8165c127de8579ab9def8c92c32dbf
OgnjenGligoric <gligoric383@gmail.com>
2024-11-11 14:55:28 +0100

    fix: pom

--- backend/wattsgood/src/main/java/com/example/WattsGood/WattsgoodApplication.java
+++ backend/wattsgood/src/main/java/com/example/WattsGood/WattsgoodApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class WattsgoodApplication {
+public class WattsGoodApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(WattsgoodApplication.class, args);
