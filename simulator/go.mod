40975b9d22800051d8f7df32f32056016241fc0d
OgnjenGligoric <gligoric383@gmail.com>
2024-11-13 01:31:01 +0100

    feat: simulator sends messages

--- simulator/go.mod
+++ simulator/go.mod
@@ -1,3 +1,5 @@
 module simulator
 
 go 1.22.2
+
+require github.com/streadway/amqp v1.1.0
--- simulator/main.go
+++ simulator/main.go
@@ -13,7 +13,7 @@ func failOnError(err error, msg string) {
 }
 
 func main() {
-    conn, err := amqp.Dial("amqp://guest:guest@localhost:5672/")
+    conn, err := amqp.Dial("amqp://admin:admin@localhost:5672/")
     failOnError(err, "Failed to connect to RabbitMQ")
     defer conn.Close()
 
@@ -42,5 +42,5 @@ func main() {
             Body:        []byte(body),
         })
     failOnError(err, "Failed to publish a message")
-    fmt.Println(" [x] Sent", body)
+    fmt.Println(" [x] Sent:", body)
 }
