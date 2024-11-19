4c3b8f2de67ed6dd5d86624efdb42ad5635c7052
OgnjenGligoric <gligoric383@gmail.com>
2024-11-19 22:11:24 +0100

    view statistics form

--- frontend/WattsGood/src/app/household/household-details/household-details.component.ts
+++ frontend/WattsGood/src/app/household/household-details/household-details.component.ts
@@ -1,18 +1,26 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
-import { NgModule } from '@angular/core';
-import {ChangeDetectionStrategy} from '@angular/core';
+import {JsonPipe} from '@angular/common';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
+import {FormControl, FormGroup, FormsModule, ReactiveFormsModule} from '@angular/forms';
+import {provideNativeDateAdapter} from '@angular/material/core';
 import {MatDatepickerModule} from '@angular/material/datepicker';
-import {MatInputModule} from '@angular/material/input';
 import {MatFormFieldModule} from '@angular/material/form-field';
-import {DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, NativeDateAdapter, provideNativeDateAdapter} from '@angular/material/core';
+import { ActivatedRoute } from '@angular/router';
+import { OwlDateTimeModule, OwlNativeDateTimeModule } from '@danielmoncada/angular-datetime-picker';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-household-details',
   standalone: true,
-  imports: [MatFormFieldModule, MatInputModule, MatDatepickerModule
-  ],
-  providers: [{provide: DateAdapter, useClass: NativeDateAdapter}, {provide: MAT_DATE_FORMATS, useValue: MAT_NATIVE_DATE_FORMATS},],
+  imports: [
+    MatFormFieldModule,
+    MatDatepickerModule,
+    FormsModule,
+    ReactiveFormsModule,
+    OwlDateTimeModule,
+    OwlNativeDateTimeModule,
+    MatButtonModule],
+  providers: [provideNativeDateAdapter()],
   templateUrl: './household-details.component.html',
   styleUrl: './household-details.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush,
@@ -20,11 +28,26 @@ import {DateAdapter, MAT_DATE_FORMATS, MAT_NATIVE_DATE_FORMATS, NativeDateAdapte
 })
 export class HouseholdDetailsComponent {
   householdId: number | null = null;
-
+  exceedesOneYearDifference: boolean = false;
+  public selectedMoments = [
+    new Date(),
+    new Date()
+  ];
   constructor(private route: ActivatedRoute) {
   }
 
   ngOnInit(): void {
     this.householdId = Number(this.route.snapshot.paramMap.get('id'));
+  }
+
+  validateDateRange(): void {
+    if (this.selectedMoments.length === 2) {
+      const [startDate, endDate] = this.selectedMoments;
+      if (startDate && endDate) {
+        const differenceInYears = Math.abs(endDate.getFullYear() - startDate.getFullYear());
+        const sameYearButExceeds = differenceInYears === 0 && endDate > new Date(startDate.getTime() + 365 * 24 * 60 * 60 * 1000);
+        this.exceedesOneYearDifference = differenceInYears > 1 || sameYearButExceeds;
+      } 
+    }
   }
 }
